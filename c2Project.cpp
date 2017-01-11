18f59b5bc24407d898028292ef33e565f0bef829
Dominik Klarkowski <Dominik Klarkowski>
2017-01-11 19:55:36 +0100

    c2proj

---
+++ c2Project.cpp
@@ -0,0 +1,55 @@
+
+
+#include "stdafx.h"
+#include <SFML/Graphics.hpp>
+#include "Map.h"
+#include "Gravity.h"
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(WINDOWS_WIDTH, WINDOWS_HEIGHT), "C2projekt");
+	window.setVerticalSyncEnabled(true);
+	window.setFramerateLimit(60);
+
+	Map map;
+	DisplayMapManager displayMapManager(&map, &window);
+	Gravity gravity(&displayMapManager);
+
+	while (window.isOpen())
+	{
+		
+		gravity.interactGravity();
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			displayMapManager.moveCharacterLeft();
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			displayMapManager.moveCharacterRight();
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			gravity.characterJump();
+		}
+		else {
+			gravity.stopJump();
+		}
+
+
+
+
+		window.clear(sf::Color(0, 0, 0));
+
+		displayMapManager.display();
+		window.display();
+	}
+}
+
